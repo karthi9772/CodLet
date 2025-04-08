404045372be2f86c6a8b48dd5c962ffe6ae40285
karthi9772 <karthi.g9772@gmail.com>
2025-04-08 22:54:13 +0530

    Routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Homepage from "./Pages/Homepage"
+import Editor from "./Pages/Editor"
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={Homepage}/>
+          <Route path="/editor" element={Editor} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
