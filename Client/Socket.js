d84314f6748f2dbced37fd0a78c8730fc8f77531
Necro-sword <gokulkumarn2k4@gmail.com>
2025-05-21 22:59:36 +0530

    final test

--- Client/Socket.js
+++
@@ -1,5 +0,0 @@
-// src/socket.js
-import { io } from "socket.io-client";
-
-// Replace with your backend server URL
-export const socket = io("http://localhost:3000"); 
--- Client/src/Components/Voice.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Voice = () => {
-  return (
-    <div>Voice</div>
-  )
-}
-
-export default Voice
